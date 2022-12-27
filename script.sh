26fc79032a6b44426761b86cd7d0739b2ce12d04
junjie1475 <junjie1475@gmail.com>
2022-12-27 05:10:11 +0000

    x

--- script.sh
+++ script.sh
@@ -2,8 +2,9 @@
 sudo apt-get -y update
 sudo add-apt-repository -y universe
 sudo apt install -y python3-proselint
-
+sudo apt-get install findutils
 # check repo
 cd "$GITHUB_WORKSPACE" || exit 1
 find . -name '*.md' | xargs proselint
+proselint typo.md
 ls
